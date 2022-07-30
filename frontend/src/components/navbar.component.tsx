5c2666fd43d08df7e392f263e69b5e01314574d6
Alexdelia <alexandre.delille.57@gmail.com>
2022-07-30 16:00:39 +0200

    add play button

--- frontend/src/components/navbar.component.tsx
+++ frontend/src/components/navbar.component.tsx
@@ -1,23 +1,22 @@
 import '../style/navbar.css';
 
 import { ReactComponent as Home } from '../icon/home-icon-silhouette-svgrepo-com.svg'
+import { ReactComponent as Play } from '../icon/ping-pong-svgrepo-com.svg'
 import { ReactComponent as Chat } from '../icon/chat-svgrepo-com.svg'
 import { ReactComponent as Profile } from '../icon/profile-user-svgrepo-com.svg'
 
 function NavBar()
 {
-	const icon_size = "50vh";
-	const fill = "white";
-
 	return (
 		<nav className='nav'>
-			<a href='/' className='nav--home'><Home width={icon_size} height={icon_size} fill={fill} /></a>
+			<a href='/'><Home /></a>
+			<a href='/play' className='nav--middle'><Play /></a>
 			<ul>
 				<li>
-					<a href='/chan'><Chat width={icon_size} height={icon_size} fill={fill} /></a>
+					<a href='/chan'><Chat /></a>
 				</li>
 				<li>
-					<a href='/profile'><Profile width={icon_size} height={icon_size} fill={fill} /></a>
+					<a href='/profile'><Profile /></a>
 				</li>
 			</ul>
 		</nav>
