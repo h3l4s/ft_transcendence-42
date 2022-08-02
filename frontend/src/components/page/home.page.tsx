37900713357d23657e1068f11f169f45e272a33b
Alexdelia <alexandre.delille.57@gmail.com>
2022-08-02 19:53:21 +0200

    button to login page

--- frontend/src/components/page/home.page.tsx
+++ frontend/src/components/page/home.page.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Home()
 {
 	return (
 		<div style={{ backgroundColor: "#96c471", color: "#fff", textAlign: "center", paddingTop: "5rem" }}>
 			home page
 			<div style={{ color: "red" }}>🚧 WIP 🚧</div>
+			<br />
+			<br />
+			<br />
+			<div style={{ paddingRight: "12rem" }}>
+				<Link to='/login' className='btn'>login page</Link>
+			</div>
 		</div>
 	);
 }
