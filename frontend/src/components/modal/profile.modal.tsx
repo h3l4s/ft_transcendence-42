e75bb0087a1447f966db057862d439d03f1aade1
Alexdelia <alexandre.delille.57@gmail.com>
2022-08-03 15:35:24 +0200

    add/remove as friend + heart, Link/icon/button to profile page in profile modal

--- frontend/src/components/modal/profile.modal.tsx
+++ frontend/src/components/modal/profile.modal.tsx
@@ -1,21 +1,45 @@
-import { useContext } from "react";
+import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
 
-import "../../style/chan.css"
+import '../../style/chan.css'
 
-import { AuthContext } from "../../context/auth.context";
+import { AuthContext } from '../../context/auth.context';
 
-import i_user from "../../interface/user.interface";
+import i_user from '../../interface/user.interface';
+
+import { ReactComponent as ProfilePage } from '../../icon/profile-user-svgrepo-com.svg';
+import { ReactComponent as AddFriend } from '../../icon/add-friend-svgrepo-com.svg';
+import { ReactComponent as RemoveFriend } from '../../icon/delete-unfriend-svgrepo-com.svg';
+import { ReactComponent as Heart } from '../../icon/heart-friend.svg';
 
 import UserStats from "../page/user/userstats.component";
 
 function ProfileModal(props: { user: i_user, onClose: () => void })
 {
 	const { username } = useContext(AuthContext);
+	const [friend, setFriend] = useState(false);
+
+	const link_to_profile = "/user/" + props.user.name
 
 	return (
 		<div className='modal--profile'>
+			<div className='modal--profile--top--icon'>
+				<Link to={link_to_profile}>
+					<ProfilePage />
+				</Link>
+				{!friend ? (
+					<button onClick={() => setFriend(true)}>
+						<AddFriend />
+					</button>
+				) : (
+					<button onClick={() => setFriend(false)}>
+						<RemoveFriend />
+					</button>
+				)}
+			</div>
 			<div>
-				<img className='img' style={{ height: "30vh", width: "30vh" }} src={props.user.profilePicPath} alt="profile" />
+				<img className='img' style={{ marginTop: "-3rem", height: "30vh", width: "30vh" }} src={props.user.profilePicPath} alt="profile" />
+				{friend && <Heart className='heart' onClick={() => setFriend(false)} />}
 			</div>
 			<div>
 				<h2 className='truncate' style={{ paddingTop: "1rem", paddingBottom: "1rem" }}>{props.user.name}</h2>
