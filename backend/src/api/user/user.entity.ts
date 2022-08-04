3acc8050b8ab257d7cfff45bb3e60c47b8a8ab9b
Alexdelia <alexandre.delille.57@gmail.com>
2022-08-04 20:28:14 +0200

    understanding backend

--- backend/src/api/user/user.entity.ts
+++ backend/src/api/user/user.entity.ts
@@ -1,23 +1,47 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
+import { i_matchHistory } from 'src/interface/matchHistory.interface';
+
 @Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  public id!: number;
+export class User
+{
+	@PrimaryGeneratedColumn()
+	public id!: number;
 
-  @Column({ type: 'varchar', length: 120 })
-  public access_token: string;
+	@Column({ type: 'varchar', length: 120 })
+	public access_token: string;
 
-  @Column({ type: 'boolean', default: false })
-  public isDeleted: boolean;
+	@Column({ type: 'varchar', length: 255 })
+	public username: string;
 
-  /*
-   * Create and Update Date Columns
-   */
+	@Column({ type: 'int', default: 0 })
+	public xp: number;
 
-  @CreateDateColumn({ type: 'timestamp' })
-  public createdAt!: Date;
+	@Column({ type: 'int', default: 1000 })
+	public elo: number;
 
-  @UpdateDateColumn({ type: 'timestamp' })
-  public updatedAt!: Date;
+	@Column({ type: 'int', default: 0 })
+	public win: number;
+
+	@Column({ type: 'int', default: 0 })
+	public lose: number;
+
+	@Column({ type: 'jsonb', nullable: true })
+	public matchHistory?: i_matchHistory[];
+
+	@Column({ type: 'int', nullable: true })
+	public friendsId?: number[];
+
+	@Column({ type: 'boolean', default: false })
+	public isDeleted: boolean;
+
+	/*
+	 * Create and Update Date Columns
+	 */
+
+	@CreateDateColumn({ type: 'timestamp' })
+	public createdAt!: Date;
+
+	@UpdateDateColumn({ type: 'timestamp' })
+	public updatedAt!: Date;
 }
