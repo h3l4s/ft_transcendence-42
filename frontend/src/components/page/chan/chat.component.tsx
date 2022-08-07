29f2cc5d7c615c5fe0772465522d7b8cdfc1ca70
Alexdelia <alexandre.delille.57@gmail.com>
2022-08-07 22:34:27 +0200

    2 more options

--- frontend/src/components/page/chan/chat.component.tsx
+++ frontend/src/components/page/chan/chat.component.tsx
@@ -10,8 +10,10 @@ import Backdrop from "../../modal/backdrop";
 function Chat(props: { chan: i_chan, users: i_user[], is_admin: boolean, is_owner: boolean })
 {
 	const [showOption, setShowOption] = useState(false);
+	const [showAdd, setShowAdd] = useState(false);
 	const [showChallenge, setShowChallenge] = useState(false);
 	const [showMute, setShowMute] = useState(false);
+	const [showAdminAdd, setShowAdminAdd] = useState(false);
 	const [showAdminBan, setShowAdminBan] = useState(false);
 	const [showAdminMute, setShowAdminMute] = useState(false);
 	const [showOwnerPwd, setShowOwnerPwd] = useState(false);
@@ -19,8 +21,10 @@ function Chat(props: { chan: i_chan, users: i_user[], is_admin: boolean, is_owne
 	function resetAllStateHandle(): void
 	{
 		setShowOption(false);
+		setShowAdd(false);
 		setShowChallenge(false);
 		setShowMute(false);
+		setShowAdminAdd(false);
 		setShowAdminBan(false);
 		setShowAdminMute(false);
 		setShowOwnerPwd(false);
@@ -41,15 +45,19 @@ function Chat(props: { chan: i_chan, users: i_user[], is_admin: boolean, is_owne
 			{showOption && <Backdrop onClick={resetAllStateHandle} />}
 			{showOption && <OptionModal
 				// also an option to quit the channel
+				setAdd={setShowAdd}
 				setChallenge={setShowChallenge}
 				setMute={setShowMute}
+				setAdminAdd={setShowAdminAdd}
 				setAdminBan={setShowAdminBan}
 				setAdminMute={setShowAdminMute}
 				setOwnerPwd={setShowOwnerPwd}
 				onClose={() => { setShowOption(false) }}
 			/>}
+			{showAdd && <PickUserModal users={props.users} type='add' onClose={() => { setShowAdd(false); setShowOption(false); }} />}
 			{showChallenge && <PickUserModal users={props.users} type='challenge' onClose={() => { setShowChallenge(false); setShowOption(false); }} />}
 			{showMute && <PickUserModal users={props.users} type='mute' onClose={() => { setShowMute(false); setShowOption(false); }} />}
+			{props.is_admin && showAdminAdd && <PickUserModal users={props.users} type='admin add' onClose={() => { setShowAdminAdd(false); setShowOption(false); }} />}
 			{props.is_admin && showAdminBan && <PickUserModal users={props.users} type='admin ban' onClose={() => { setShowAdminBan(false); setShowOption(false); }} />}
 			{props.is_admin && showAdminMute && <PickUserModal users={props.users} type='admin mute' onClose={() => { setShowAdminMute(false); setShowOption(false); }} />}
 			{props.is_owner && showOwnerPwd && <PickPwd users={props.users} type='owner pwd' onClose={() => { setShowOwnerPwd(false); setShowOption(false); }} />}
