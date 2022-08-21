c10dc8ea356d981fdf1401d35b1c6d15d3826484
pthink <mehdiyuva@protonmail.com>
2022-08-21 16:43:36 +0200

    3

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
 	
 	getHello(): string {
-		//create a default user with a pid of 1
-		//this.UserService.tmpCreateUser({ name: "default" })
 		return JSON.stringify("Hello World from Backend!");
 	}
 }
--- frontend/src/components/CreateDefaultUser.tsx
+++ frontend/src/components/CreateDefaultUser.tsx
@@ -6,57 +6,26 @@ import { getEnvironmentData } from "worker_threads";
 import { getDialogTitleUtilityClass, TableRow } from "@mui/material";
 import React from "react";
 
-/*function getData() {
-    const res = await  axios.get("http://localhost:3000/user/");
-    return res.json();
-    
-    axios.get("http://localhost:3000/user/").then(
-        (res) => { console.log(res.data); }
-    );
-}*/
-
 function CreateDefaultUser()
 {
 	// if no user with id == 1
     // create default user with id 1
 
     const axios = require('axios');
-    const res = axios.get("http://localhost:3000/user/").then(
-        (response: any) => {
-            this.setState({response: response})
-        }).catch( (error: any) => {
-            console.log(error);
-        })
-
-
-     export default function App() {
-        const [post, setPost] = React.useState(null);
-         
-        React.useEffect(() => {
-            axios.get("http://localhost:3000/user/").then((response) => {
-                setPost(response.data);
-            });
-        }, []);
-
-    if(!post)
+    const res = axios.get("http://localhost:3000/user/").then(res =>
     {
-      //  leng = res.data.id;
-        axios.post("http://localhost:3000/user/name/default");
+        console.log(res);
+        if(res.data.length === 0)
+        {
+            axios.post("http://localhost:3000/user/name/default");
+        }
     }
-    console.log(res.data);
-
-    //axios.post("http://localhost:3000/user/name/default", formData).then(
-	//	(res) => { console.log(res); },
-	//	(error) => { console.log(error); }
-	//);
+    );
     return (
         <div>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
         </div>
-      );
+    );
     
 }
 
 export default CreateDefaultUser;
-}
