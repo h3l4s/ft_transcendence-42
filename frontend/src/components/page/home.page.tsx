5193894ff7b9986d82d27d9175b044866fe7fd7b
helaski <jesuisformule1@gmail.com>
2022-09-14 14:44:27 +0200

    navbar alt version/home page changes

--- frontend/src/components/page/home.page.tsx
+++ frontend/src/components/page/home.page.tsx
@@ -13,7 +13,7 @@ function Home()
 					display: "flex", flexDirection: "column", justifyContent: "space-around",
 					height: "75%", width: "60%", margin: "auto", padding: "5rem 0 5rem 0"
 				}}>
-				<div style={{ fontFamily: "var(--alt-font)", fontSize: "11rem", textShadow: "10px 10px 3px #0008" }}>pong</div>
+				<div style={{ fontFamily: "var(--alt-font)", fontSize: "2.8rem", textShadow: "10px 10px 3px #0008" }}>ft_transcendence</div>
 				<div style={{ paddingRight: "12rem" }}>
 					<Link to='/login' className='btn'>login page</Link>
 				</div>
