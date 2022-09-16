aece57ddade922a091aa400e76c2bce72b49b2e2
adelille <adelille@student.42.fr>
2022-09-16 14:59:25 +0200

    ChallengePage is crashing pc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -67,7 +67,6 @@ function App()
 							<Route path="/connect/:token" element={<><NavBar /><ConnectPage /></>} />
 							<Route path="/play" element={<RequireAuth><><NavBar /><PongPage /></></RequireAuth>} />
 							<Route path="/pong/:type" element={<RequireAuth><Pong /></RequireAuth>} />
-							<Route path="/challenge/:senderId/:receiverId" element={<RequireAuth><ChallengePage /></RequireAuth>} />
 							<Route path="/chan" element={<RequireAuth><><NavBar /><ChanPage /></></RequireAuth>} />
 							<Route path="/user" element={<RequireAuth><><NavBar /><UserPage /></></RequireAuth>} />
 							<Route path="/user/:username" element={<><NavBar /><UserPage /></>} />
@@ -80,5 +79,6 @@ function App()
 		</Router >
 	);
 }
+//<Route path="/challenge/:senderId/:receiverId" element={<RequireAuth><ChallengePage /></RequireAuth>} />
 
 export default App;
