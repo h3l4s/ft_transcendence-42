bcd799aef37eb878ebb8a467f717b4c9cffbfaf9
Ilyes DHIBA <idhiba@e1r6p9.clusters.42paris.fr>
2022-09-16 17:43:11 +0200

    be back version

--- backend/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-	getHello(): string {
-		return JSON.stringify("Hello World from Backend!");
-	}
-}
--- frontend/src/components/request_answer_component/success.component.tsx
+++
@@ -1,6 +0,0 @@
-function Success(props: { code: number })
-{
-	return (<div style={{ color: "yellowgreen" }}>{props.code} : success!</div>);
-}
-
-export default Success;
